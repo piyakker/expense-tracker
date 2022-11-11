418944025e33b54af8830ae57704ce5665e49b24
piyakker <ericlien86@gamil.com>
2022-11-11 13:47:37 +0800

    connect to db

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 3000
 const routes = require('./routes/index')
 
+//連線資料庫
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+require('./config/mongoose')
+
 app.use('/', routes)
 
 app.listen(port, () => {
