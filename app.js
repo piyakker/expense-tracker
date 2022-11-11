ba10936d1898408c84aabeb12ce43afe52aabd6d
piyakker <ericlien86@gamil.com>
2022-11-11 16:15:55 +0800

    add methodOverride

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
@@ -19,6 +20,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use('/', routes)
 
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const { route } = require('./home')
 
 router.get('/new', (req, res) => {
   res.render('new')
@@ -11,6 +12,18 @@ router.post('/', (req, res) => {
   return Record.create(req.body)
   .then(() => res.redirect('/'))
   .catch(err => console.log(err))
+})
+
+route.get('/:record_id/edit', (req, res) => {
+  res.send('edit page')
+})
+
+route.put('/:record_id', (req, res) => {
+  res.send('update record')
+})
+
+route.delete('/:record_id', (req, res) => {
+  res.send('delete record')
 })
 
 module.exports = router
