5279f68c8d9611932099e9c90eea5275041f28e6
piyakker <ericlien86@gamil.com>
2022-11-11 14:43:15 +0800

    add get all records function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
+const dayjs = require('dayjs')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  return Record.find({})
+  .lean()
+    .then(records => {
+      let totalAmount = 0
+      records.forEach(record => {
+        totalAmount += record.amount
+        record.date = dayjs(record.date).format('YYYY-MM-DD')
+      })
+      res.render('index', { records, totalAmount })
+    })
 })
 
 module.exports = router
