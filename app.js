bf00f559cff591f6146aea4d921eaac77b889f08
piyakker <ericlien86@gamil.com>
2022-11-11 14:18:08 +0800

    add index page

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express =require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
 const port = 3000
 const routes = require('./routes/index')
@@ -8,6 +10,13 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 require('./config/mongoose')
+
+//引用handlebars
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
 
 app.use('/', routes)
 
