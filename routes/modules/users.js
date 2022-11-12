552992e17411ae089e0d6200de904dd2e9136e7b
piyakker <ericlien86@gamil.com>
2022-11-12 13:21:43 +0800

    add register check function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,10 +15,24 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  const {name, email, password} = req.body
-  return User.create({ name, email, password })
-  .then(() => res.redirect('/users/login'))
-  .catch(err => console.log(err))
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({ name, email, password })
+          .then(() => res.redirect('/users/login'))
+          .catch(err => console.log(err))
+      }
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
