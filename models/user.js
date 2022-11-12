8857162eecf893ce4f42ce64fa9c0f1e9f364fea
piyakker <ericlien86@gamil.com>
2022-11-12 13:14:33 +0800

    add userCounter model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const counterSchema = Schema({
-  _id: { type: String, required: true },
-  seq: { type: Number, default: 0 }
-});
-const Counter = mongoose.model('Counter', counterSchema);
+const autoIncrementModelID = require('./userCounter')
 
-const userSchema = {
+const userSchema = Schema({
   _id: {
-    type: Number,
-    required: true
+    type: Number
   },
   name: {
     type: String,
@@ -28,15 +23,14 @@ const userSchema = {
     type: Date,
     default: Date.now
   },
-}
+})
 
   userSchema.pre('save', function (next) {
-    Counter.findByIdAndUpdate({ _id: 'entityId' }, { $inc: { seq: 1 } }, function (error, counter) {
-      if (error)
-        return next(error);
-      this._id = counter.seq;
+    if (!this.isNew) {
       next();
-    });
+      return;
+    }
+    autoIncrementModelID('activities', this, next)
   })
 
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -14,7 +15,10 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('register page')
+  const {name, email, password} = req.body
+  return User.create({ name, email, password })
+  .then(() => res.redirect('/users/login'))
+  .catch(err => console.log(err))
 })
 
 module.exports = router
