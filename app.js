a54ae94b8c5721d6488eeb5992c365c9482f7c07
piyakker <ericlien86@gamil.com>
2022-11-12 13:37:21 +0800

    add login function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -45,6 +46,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
+usePassport(app)
 
 app.use('/', routes)
 
