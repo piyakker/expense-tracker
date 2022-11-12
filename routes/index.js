3ff89d90db3a3a9a65f4b1fc7c9bf9ff894326c3
piyakker <ericlien86@gamil.com>
2022-11-12 13:43:24 +0800

    add user authentication

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const { authenticator } = require('../middleware/auth')
 
 const homeRouter = require('./modules/home')
 const recordRouter = require('./modules/records')
 const userRouter = require('./modules/users')
 
-router.use('/', homeRouter)
-router.use('/records', recordRouter)
+router.use('/records',authenticator, recordRouter)
 router.use('/users', userRouter)
+router.use('/',authenticator, homeRouter)
 
 
 module.exports = router
